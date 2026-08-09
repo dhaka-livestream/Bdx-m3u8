d3a7374e9513b1370d3a536de2002617d35b893e
dhaka-livestream <146126511+dhaka-livestream@users.noreply.github.com>
2026-08-09 19:16:15 +0600

    Add new streaming source for colorsbdcinema

--- _worker.js
+++ _worker.js
@@ -33,6 +33,10 @@ export default {
         url: 'http://103.165.93.31:8095/jalshaMovies/tracks-v1a1/mono.m3u8',
         type: 'redirect'
       },
+      'colorsbdcinema-sdr': {
+        url: 'http://live.balajibroadband.com:3500/live/1657.m3u8',
+        type: 'redirect'
+      },
       'zeebanglahd-bdx1': {
         url: 'http://103.165.93.31:8095/zeeBangla/tracks-v1a1/mono.m3u8',
         type: 'redirect'
